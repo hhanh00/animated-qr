793c52b81a96a832ad4f94a4f286da3d15ebf0e5
hhanh00 <hanh425@gmail.com>
2025-08-22 18:27:59 +0800

    feat: more settings (#7)

--- lib/store.dart
+++ lib/store.dart
@@ -12,12 +12,16 @@ abstract class StoreBase with Store {
 
   @observable int type = 20;
   @observable int errorLevel = 1;
+  @observable int delay = 500;
+  @observable int repair = 2;
 
   @action
   Future<void> save() async {
     final prefs = SharedPreferencesAsync();
     await prefs.setInt("type", type);
     await prefs.setInt("errorLevel", errorLevel);
+    await prefs.setInt("delay", delay);
+    await prefs.setInt("repair", delay);
   }
 
   @action
@@ -25,5 +29,7 @@ abstract class StoreBase with Store {
     final prefs = SharedPreferencesAsync();
     type = await prefs.getInt("type") ?? 20;
     errorLevel = await prefs.getInt("errorLevel") ?? 1;
+    delay = await prefs.getInt("delay") ?? 500;
+    repair = await prefs.getInt("repair") ?? 2;
   }
 }
